f67b7603ef75833be9fd66550dd451ccc6c46f66
Joerg Wangemann <joerg.wangemann@gmx.de>
2018-10-21 23:26:18 +0200

    Parsed the PDF to improve the integrity of the "final_project_dataset.pkl". TODO: save modifications to "final_project_dataset_PROVEN.pkl"

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-""" 
+"""
     Starter code for exploring the Enron dataset (emails + finances);
     loads up the dataset (pickled dict of dicts).
 
@@ -12,11 +12,30 @@
     but here's an example to get you started:
 
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
+
 """
 
 import pickle
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
+print(len(enron_data['METTS MARK']))
 
+cnt=0
+for p_id, p_info in enron_data.items():
+    if p_info['poi'] == 1:
+        print("\nPerson ID:", p_id)
+        cnt+=1
 
+    #for key in p_info:
+    #    print(key + ':', p_info[key])
+
+print("Number of POI:{0}".format(cnt))
+
+stockval = enron_data["PRENTICE JAMES"]["total_stock_value"]
+print("What is the total value of the stock belonging to James Prentice? {0}".format(stockval))
+
+num_emails_from_this_person_to_poi = enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+print("How many email messages do we have from Wesley Colwell to persons of interest? {0}".format(num_emails_from_this_person_to_poi))
+
+stock_options = enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+print("What’s the value of stock options exercised by Jeffrey K Skilling? {0}".format(stock_options))
--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python
 
+import os
 import sys
 import pickle
+import numpy as np
+import pandas as pd
+from copy import deepcopy
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection  import train_test_split
+from sklearn.metrics          import accuracy_score, confusion_matrix, precision_score, recall_score, classification_report
+from sklearn.tree             import DecisionTreeClassifier
+
+from collections import Counter
+
+
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
@@ -10,16 +23,59 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_email_stats = ['from_messages','from_this_person_to_poi', 'to_messages','from_poi_to_this_person', 'shared_receipt_with_poi']
+features_financial_salary = ['salary', 'bonus', 'long_term_incentive', 'deferred_income', 'deferral_payments', 'loan_advances', 'other', 'expenses', 'director_fees']
+features_financial_salary_total = ['total_payments']
+features_financial_stock = ['exercised_stock_options', 'restricted_stock', 'restricted_stock_deferred']
+features_financial_stock_total = ['total_stock_value']
+features_email = ['fraction_from_poi_to_this_person', 'fraction_from_this_person_to_poi'] # This is designed in Task 3
+
+features_list = ['poi'] + ['salary', 'bonus'] # You will need to use more features
+
 
 ### Load the dictionary containing the dataset
-with open("final_project_dataset.pkl", "r") as data_file:
+with open("final_project_dataset.pkl", "rb") as data_file:
     data_dict = pickle.load(data_file)
 
+# Some utility functions do not like mixed types
+for p_id, p_info in data_dict.items():
+    # p_info['poi'] = float(int(p_info['poi']))
+    #if p_info['poi']:
+    #    p_info['poi'] = True
+    #else:
+    #    p_info['poi'] = False
+    if p_info['email_address'] == 'NaN':
+        p_info['email_address'] = 'unknown'
+    for info in p_info:
+        if p_info[info] == 'NaN':
+            p_info[info] = np.nan
+
+data_dict_raw = deepcopy(data_dict)
+
 ### Task 2: Remove outliers
+df = pd.DataFrame.from_dict(data_dict_raw, orient='index').replace('NaN', np.nan)
+toomanynan = df.isna().sum(axis=1) > 16
+df_toomanynan = df[toomanynan]
+
+outliers2remove = df_toomanynan.index.values.tolist() + [df['salary'].idxmax()]
+
+for pos in outliers2remove:
+    data_dict.pop(pos, 0) # remove them from the data_dict
+
+
+
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
-my_dataset = data_dict
+my_dataset = deepcopy(data_dict)
+
+# Let the fraction of emails from and to a person of interest be new (human readable) features.
+for p_id, p_info in my_dataset.items():
+    p_info['fraction_from_poi_to_this_person'] = p_info['from_poi_to_this_person'] / p_info['to_messages']
+    p_info['fraction_from_this_person_to_poi'] = p_info['from_this_person_to_poi'] / p_info['from_messages']
+    for info in p_info:
+        if p_info[info] != p_info[info]: # NaN is always != NaN
+            p_info[info] = 0.0
+
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
@@ -35,15 +91,16 @@ labels, features = targetFeatureSplit(data)
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+### Task 5: Tune your classifier to achieve better than .3 precision and recall
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
 ### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
+### stratified shuffle split cross validation. For more info:
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
+# from sklearn.cross_validation import train_test_split
+from sklearn.model_selection  import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
